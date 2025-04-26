5955063858bc5530eba5651bb0dc2869a900b64f
adminmodeva <admin@modeva.ai>
2025-04-27 00:38:38 +0800

    update galleries

--- docs/_build/html/_downloads/a872fe3c33bd9b8fb698ef390e4228cf/plot_5_lineartree_reg.py
+++ docs/_build/html/_downloads/a872fe3c33bd9b8fb698ef390e4228cf/plot_5_lineartree_reg.py
@@ -15,7 +15,7 @@ Linear Tree Regression
 
 # To get authentication, use the following command: (To get full access please replace the token to your own token)
 # from modeva.utils.authenticate import authenticate
-# authenticate(token='eaaa4301-b140-484c-8e93-f9f633c8bacb')
+# authenticate(auth_code='eaaa4301-b140-484c-8e93-f9f633c8bacb')
 
 # %%
 # Import required modules
